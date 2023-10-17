18b65c9bfbef5efec3f879d0c21c5efffa5ba024
bianavic <bianavic@gmail.com>
2023-10-17 10:59:53 -0300

    [aula 1]: anatomia das classes

---
+++ src/main/java/org/example/MinhaClasse.java
@@ -0,0 +1,21 @@
+package org.example;
+
+// CLASSE - toda classe precisa existir dentro diretorio src
+public class MinhaClasse {
+
+    // METODO
+    public static void main(String[] args) {
+        // classe executavel
+        // void = só irá executar, nao retorna nada
+        // () = metodos
+        // parametros é um arrei do tipo String[] com o parametro chamado args
+        // {} = dentro do metodo há um corpo
+
+        // CLASSE
+        // println() = metodo
+        // parametros que permitem que o metodo seja executado
+        System.out.println("Hello stranger, welcome!");
+
+    }
+
+}
