dbd160d92f4b3c283af9ffd36566edefc656ec98
bianavic <bianavic@gmail.com>
2023-10-17 11:35:29 -0300

    [aula 5]: organizacao de arquivos

--- src/main/java/edu/fabs/primeirasemana/AnatomiaDasClassesTres.java
+++ src/main/java/edu/fabs/primeirasemana/AnatomiaDasClassesTres.java
@@ -1,4 +1,4 @@
-package org.example;
+package edu.fabs.primeirasemana;
 
 public class AnatomiaDasClassesTres {
 
--- src/main/java/edu/fabs/primeirasemana/AnatomiaDasClassesUm.java
+++ src/main/java/edu/fabs/primeirasemana/AnatomiaDasClassesUm.java
@@ -1,4 +1,4 @@
-package org.example;
+package edu.fabs.primeirasemana;
 
 // CLASSE - toda classe precisa existir dentro diretorio src
 public class AnatomiaDasClassesUm {
