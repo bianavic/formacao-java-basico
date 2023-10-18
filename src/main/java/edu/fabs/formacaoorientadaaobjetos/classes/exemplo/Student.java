e894034194e7e50ebe9170449e30bff768b143f3
bianavic <bianavic@gmail.com>
2023-10-18 17:27:02 -0300

    aula sobre classes

---
+++ src/main/java/edu/fabs/formacaoorientadaaobjetos/classes/exemplo/Student.java
@@ -0,0 +1,20 @@
+//package edu.fabs.formacaoorientadaaobjetos.classes.exemplo;
+//
+//import java.awt.*;
+//
+//// Criando a classe Student
+//public class Student {
+//
+//    // características e compartamentos
+//    String name;
+//    int age;
+//    Color color;
+//    Sex sex;
+//
+//    void eating(Lunch lunch) {}
+//
+//    void drinking(Juice juice) {}
+//
+//    void running() {}
+//
+//}
